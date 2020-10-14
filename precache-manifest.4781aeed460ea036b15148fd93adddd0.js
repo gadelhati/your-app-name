ff3443540ced7bfcc9803920d7511a87641a87ee
00038059 <marcelo.gadelha@marinha.mil.br>
2020-10-14 10:41:02 -0300

    Updates

---
+++ precache-manifest.4781aeed460ea036b15148fd93adddd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44db0b5b9abeb0171ad3d59702186f54",
+    "url": "/gadelhati/your-app-name/index.html"
+  },
+  {
+    "revision": "9df88993f4db09cf9671",
+    "url": "/gadelhati/your-app-name/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d645dc49f3c04a6cc82e",
+    "url": "/gadelhati/your-app-name/static/js/2.4b62b853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gadelhati/your-app-name/static/js/2.4b62b853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df88993f4db09cf9671",
+    "url": "/gadelhati/your-app-name/static/js/main.64b6da8a.chunk.js"
+  },
+  {
+    "revision": "f4065a534b2f244be15d",
+    "url": "/gadelhati/your-app-name/static/js/runtime-main.9cfe9272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gadelhati/your-app-name/static/media/logo.5d5d9eef.svg"
+  }
+]);
